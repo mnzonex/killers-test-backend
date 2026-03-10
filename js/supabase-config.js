d42f94a405d3a7865d3651c9cf2f17f4b22271c2
MN ZONEX 2.0 <madhushanimsara849@gmail.com>
2026-03-10 20:26:27 +0530

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,9 +1,10 @@
 // Supabase Configuration Template
 // Replace with your real Supabase URL and Key
-const SUPABASE_URL = 'https://your-supabase-url.supabase.co';
-const SUPABASE_ANON_KEY = 'your-supabase-anon-key';
+const SUPABASE_URL = 'https://qvytyninfmuykjbhhnbg.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2eXR5bmluZm11eWtqYmhobmJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNTA1NTcsImV4cCI6MjA4ODcyNjU1N30.2KnhpHbDAE2C1wnBYSf_cwNrYQw7nKng6x-cZwSbFbk';
 
 const { createClient } = supabase;
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 window.supabaseClient = supabaseClient;
+
